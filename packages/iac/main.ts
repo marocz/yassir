024a2d58cfb4fc92321a7b5fa8ef7d1fb5208735
marocz <mario.maro111@gmail.com>
2023-11-24 01:22:23 +0100

    added Coffee and Ingredients

--- packages/iac/main.ts
+++ packages/iac/main.ts
@@ -1,59 +1,135 @@
-// import { Construct } from "constructs";
-// import { App, TerraformStack } from "cdktf";
-// import { provider } from '@cdktf/provider-hashicups';
-// import { dataHashicupsCoffees } from '@cdktf/provider-hashicups'
 
-import { provider } from '@cdktf/provider-hashicups';
-import { App, TerraformStack } from 'cdktf';
+import { dataHashicupsCoffees, dataHashicupsIngredients, provider } from '@cdktf/provider-hashicups';
+import { App, TerraformDataSource, TerraformStack } from 'cdktf';
 import { Construct } from 'constructs';
-//import { CoffeeType, CoffeeTypeConfig } from './src/models/coffee';
-//import { CoffeeOrder, OrderConfig } from './src/models/orders';
 
+
+
+export interface Ingredient {
+    id: string;
+    name: string;
+    quantity: number;
+  }
+  //const coffeeTypes = ["espresso","cappucino"];
+  interface Coffee {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    ingredients: Ingredient[];
+  }
+  interface RawCoffee {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    ingredients: any[]; // Adjust this type based on the actual structure of ingredients
+  }
+  
+  
+export class MyCoffeeStand extends TerraformDataSource {
+
+    private dataHashicupsCoffees: dataHashicupsCoffees.DataHashicupsCoffees;
+    public coffees: Coffee[]=[];
+    constructor(scope: Construct, name: string ){//{ ingredientIds: string[] }) {
+      super(scope, name,  {
+        terraformResourceType: 'hashicups_coffees',
+      });
+   
+      this.dataHashicupsCoffees = new dataHashicupsCoffees.DataHashicupsCoffees(this, `HashicupsCoffee_${name}`);
+  
+      this.coffees = this.processCoffees();
+    }
+  
+    private processCoffees(): Coffee[] {
+ 
+          // Fetch coffees from data source
+    // const rawCoffees = this.dataHashicupsCoffees.coffees; // Assuming this is how you access the raw data
+    // const rawCoffeesArray: RawCoffee[] = [];
+    try {
+        
+        
+        for (let i = 0; i < 3; i++) {
+        const coffeeRef = this.dataHashicupsCoffees.coffees.get(i);
+        if (coffeeRef) {
+            const ingredientIds: number[] = [];
+
+            for (let j = 0; i < coffeeRef.ingredients.get.length; j++) {
+                const ingredientRef = coffeeRef.ingredients.get(j);
+                if (ingredientRef && ingredientRef.ingredientId) {
+                  // Add the ingredient ID to the array
+                  ingredientIds.push(ingredientRef.ingredientId);
+                }
+              }
+          const rawCoffee: RawCoffee = {
+            id: coffeeRef.id.toString(),
+            name: coffeeRef.name,
+            description: coffeeRef.description,
+            price: coffeeRef.price,
+            ingredients: this.processIngredients(ingredientIds)
+          };
+  
+          this.coffees.push(rawCoffee);
+          
+
+// Iterate over the ingredients list and extract the ingredient IDs
+
+        }
+    }
+    } catch (error) {
+      // Reached the end of the list or encountered an error
+    }
+    // Process raw data into Coffee array
+    return this.coffees;
+   
+    }
+    private processIngredients(ingredientIds: number[]): Ingredient[] {
+        const ingredients: Ingredient[] = [];
+      
+        ingredientIds.forEach(ingredientId => {
+          const ingredientDataSource = new dataHashicupsIngredients.DataHashicupsIngredients(this, `ingredient_${ingredientId}`, {
+            coffeeId: ingredientId
+          });
+      
+          // Iterate over the ingredients list and extract details
+          for (let i = 0; i < ingredientDataSource.ingredients.get.length; i++) {
+            const ingredientRef = ingredientDataSource.ingredients.get(i);
+      
+            if (ingredientRef) {
+              ingredients.push({
+                id: ingredientRef.id.toString(),
+                name: ingredientRef.name,
+                quantity: ingredientRef.quantity
+              });
+            }
+          }
+        });
+      
+        return ingredients;
+      }
+      
+   
+      
+    
+  }
 class HashiCupsStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+   constructor(scope: Construct, name: string) {
+     super(scope, name);
 
     // Initialize HashiCups provider
     new provider.HashicupsProvider(this, 'hashicups', {
       host: 'http://localhost:19090'
 
     });
+    
+      new MyCoffeeStand(this, `Coffee`);
+      new MyCoffeeStand(this, `Espresso`);
+     
 
-    // Define ingredients
-    // const espressoIngredients: Ingredient[] = [
-    //   { id: '1', name: 'Coffee', quantity: 1 },
-    //   { id: '2', name: 'Water', quantity: 1 }
-    // ];
+      
 
-    // const latteIngredients: Ingredient[] = [
-    //   { id: '1', name: 'Coffee', quantity: 1 },
-    //   { id: '3', name: 'Milk', quantity: 2 }
-    // ];
-
-  //   // Define coffee types with ingredients
-  //   const espressoConfig: CoffeeTypeConfig = {
-  //     name: 'Espresso',
-  //     description: 'Strong and dark coffee',
-  //     price: 3,
-  //     ingredients: espressoIngredients
-  //   };
-  //   new CoffeeType(this, 'espresso', espressoConfig);
-
-  //   const latteConfig: CoffeeTypeConfig = {
-  //     name: 'Latte',
-  //     description: 'Milk coffee',
-  //     price: 4,
-  //     ingredients: latteIngredients
-  //   };
-  //   new CoffeeType(this, 'latte', latteConfig);
-
-  //   // Create an order
-  //   const orderConfig: OrderConfig = {
-  //     coffeeId: 'espresso',
-  //     quantity: 2
-  //   };
-  //   new CoffeeOrder(this, 'order1', orderConfig);
-   }
+    }
+    
 }
 
 const app = new App();
--- packages/iac/src/models/ingredients.ts
+++
@@ -1,6 +0,0 @@
-export interface Ingredient {
-    id: string;
-    name: string;
-    quantity: number;
-  }
-  
